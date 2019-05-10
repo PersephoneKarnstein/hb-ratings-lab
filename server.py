7eef6812fb947e3fa43d2e61924ea24dd29b8e45
Hackbright Student <no-reply@hackbrightacademy.com>
2019-05-09 18:05:36 -0700

    MORE PRIDE FLAG BUTTONS oh and also logout/login work now i guess

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/', methods=["GET", "POST"])
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
 
 
 @app.route("/users")
@@ -39,12 +39,16 @@ def receive_email():
 
     return render_template("receive-email.html")
 
+
+
 @app.route("/check-email", methods=['POST'])
 def check_email():
-    email = request.form.get("email", "Error")
+    email = request.form.get("email", "Error") #it will return an error if it can't find them
+    session["email"] = email
     print(email)
     try:
-        user_with_email = db.session.query(User).filter_by(email = email).one() #at most we shoule find one person with this email
+        user_with_email = db.session.query(User).filter_by(email = email).one() 
+        #at most we shoule find one person with this email
         print(user_with_email)
         testEmail = {'exists':True,
         'alert':"Welcome back!\nRedirecting to login"
@@ -60,13 +64,61 @@ def check_email():
     except Exception:
         raise "There is more than one user registered with that email!!"
 
+
+
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", )
 
 @app.route("/sign-up")
 def sign_up():
     return render_template("sign-up.html")
+
+
+
+@app.route("/process-login", methods=["POST"])
+def check_success():
+    attempt_type = request.form.get("attempt-type")
+    user_email = request.form.get("email")
+
+    print(attempt_type, user_email)
+
+    # user_email = db.session.query(User).filter_by(email = email).one() #at most we shoule find one person with this email
+    
+
+    if attempt_type == "login": #so nominally, if they get to 
+        #this page, check_email() has already determined that there
+        #is a person with this email in the database
+        user = db.session.query(User).filter_by(email = user_email).first()
+        
+        #the case where there is more than one person with that email (or
+        # 0 people with that email) has already been caught by check_email()
+        if user.password == request.form.get("password"):
+            flash('You were successfully logged in')
+            return redirect("/")
+        else: 
+            flash("That password is incorrect.")
+            return redirect("/login")
+
+
+    elif attempt_type == "signup": #there is *not* a person with this
+        #name already in the database
+    
+        #add to database
+        user = User(email=user_email,
+                    password=request.form.get("password"),
+                    age=int(request.form.get("age")),
+                    zipcode=request.form.get("zipcode"))
+
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you've been signed up. I'm so excited.")
+        return redirect("/")
+
+    else:
+        raise "Invalid attempt type."
+
+    
 
 
 if __name__ == "__main__":
